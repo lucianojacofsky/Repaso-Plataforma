aa2a05aea4af973ffdeefc5a3585a9cd89120b81
lucianojacofsky <101577485+lucianojacofsky@users.noreply.github.com>
2022-05-23 10:28:23 -0300

    Finished

---
+++ Assets/PlatformScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour
+{
+
+    public float movementspeed;
+    public float derlimit;
+    public float izqlimit;
+    public bool derecha;
+    // Start is called before the first frame update
+    void Start()
+    {
+        derecha = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+        if (derecha == true)
+        {
+            transform.position += new Vector3(movementspeed, 0, 0);
+        }
+        else
+        {
+            transform.position -= new Vector3(movementspeed, 0, 0);
+        }
+            if (transform.position.x > derlimit)
+        {
+               derecha = false;
+        }
+            else if (transform.position.x < izqlimit)
+        {
+            derecha = true;
+        }
+    }
+}
